fe49c08bdf5c2a6b3e0f2079e19de8c9c6e83eda
Pinn thanida <Thanida2.decha@live.uwe.ac.uk>
2024-04-13 18:49:38 +0100

    add horses noise interaction

---
+++ Assets/script2/twohorses.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twohorses : MonoBehaviour
+{
+     
+    public AudioClip horses;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("screaming");
+        GetComponent<AudioSource>().PlayOneShot(horses);
+    }
+
+}
