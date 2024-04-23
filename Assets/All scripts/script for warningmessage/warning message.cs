01eccdc58438d2d08e239fb3d1f772ff2b18bef4
Pinn thanida <Thanida2.decha@live.uwe.ac.uk>
2024-04-23 10:39:18 +0100

    add UI, delete the chicken

--- Assets/All scripts/script for warningmessage/warning message.cs
+++ Assets/All scripts/script for warningmessage/warning message.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chicken : MonoBehaviour
+public class warningmessage : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/All scripts/tiger/CannonFire.cs
+++ Assets/All scripts/tiger/CannonFire.cs
@@ -6,6 +6,7 @@ public class CannonFire : MonoBehaviour
 {
     
     public AudioClip tiger;
+    
     // Start is called before the first frame update
     void Start()
     {
