354e05a564e02d439bf44bfbc3f91ec9dfb3d17a
Pinn thanida <Thanida2.decha@live.uwe.ac.uk>
2024-04-24 23:20:36 +0100

    add UI canvas interaction

--- Assets/All scripts/people talking/peopletalking.cs
+++ Assets/All scripts/people talking/peopletalking.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class peopletalking : MonoBehaviour
 {
     public AudioClip people;
+    public GameObject ui;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideUI();
     }
 
     // Update is called once per frame
@@ -19,9 +20,31 @@ public class peopletalking : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         
+       ShowUI();
+
+
+    }
+
+    public void agree()
+    {
         Debug.Log("talking");
         GetComponent<AudioSource>().PlayOneShot(people);
+    }
+    public void HideUI()
+    {
+     ui.SetActive(false);
+     Cursor.lockState = CursorLockMode.Locked;
+     Cursor.visible = false;
+    }
+    public void ShowUI()
+    {
+        ui.SetActive(true);
 
-
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void PressAgree(){
+        HideUI();
+        
     }
 }
